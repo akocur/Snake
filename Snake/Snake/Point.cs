2f3810010e512ddee1f5dc8c610fdd8ed30469a3
akocur <akocur@yandex.ru>
2022-08-23 17:47:00 +0300

    add Point class

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class Point
+    {
+        public int x1;
+        public int y1;
+        public char symbol;
+
+        public void Drow() 
+        {
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(symbol);
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Snake;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Point p1 = new()
+        {
+            x1 = 1,
+            y1 = 3,
+            symbol = '*'
+        };
+        p1.Drow();
+
+        Point p2 = new()
+        {
+            x1 = 4,
+            y1 = 5,
+            symbol = '#'
+        };
+        p2.Drow();
+
+        Console.ReadLine();
+    }
+}
