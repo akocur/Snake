56d82725e618ac5fb4a9a3828a341f6fabd82455
akocur <akocur@yandex.ru>
2022-08-24 11:09:05 +0300

    add Snake class

---
+++ Snake/Snake/Snake.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class Snake : Figure
+    {
+        public Snake(Point tail, int length, Direction direction)
+        {
+            for (int i=0; i<length; i++)
+            {
+                Point point = new(tail);
+                point.Move(i, direction);
+                points.Add(point);
+            }
+        }
+
+    }
+}
