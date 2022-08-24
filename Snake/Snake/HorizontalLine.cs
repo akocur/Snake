778dbd9328c42421e1d9371d3d2d381884dcf3e4
akocur <akocur@yandex.ru>
2022-08-24 09:32:17 +0300

    add HorizontalLine class

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class HorizontalLine
+    {
+        private List<Point> points;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char symbol)
+        {
+            points = new List<Point>();
+            for(int x = xLeft; x <= xRight; x++)
+            {
+                Point point = new(x, y, symbol);
+                points.Add(point);
+            }
+        }
+
+        public void Drow()
+        {
+            foreach(Point point in points)
+            {
+                point.Drow();
+            }
+        }
+    }
+}
